ed2529c4eba70621201f93aa0e22f739135d2a89
jacr1975 <jacr1975manizales@yahoo.com>
2022-03-27 15:43:17 -0500

    Primer commit

---
+++ src/main/java/com/example/demo/controller/controlador.java
@@ -0,0 +1,17 @@
+package com.example.demo.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+@RequestMapping("/example")
+public class controlador {
+
+
+		
+		@GetMapping("/hello-world")
+		public ResponseEntity<String> get() {
+			return ResponseEntity.ok("Hello World!");
+		}
+	}
